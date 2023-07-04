38e9b13020f2dd9b53a00871756dc63fadf8b4f8
Max.S <max.siemkin@pm.me>
2023-07-04 21:03:47 +0300

    1st init commit

--- helpers/int64_sort.go
+++
@@ -1,7 +0,0 @@
-package tools
-
-type Int64Sort []int64
-
-func (a Int64Sort) Len() int           { return len(a) }
-func (a Int64Sort) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a Int64Sort) Less(i, j int) bool { return a[i] < a[j] }
--- helpers/ptr.go
+++
@@ -1,6 +0,0 @@
-package tools
-
-import "time"
-
-func StrPtr(s string) *string        { return &s }
-func TimePtr(t time.Time) *time.Time { return &t }
